1ef4fbf7b8d0e28f63f1d5f0f1f36df44129d138
Gordon Evans <gordon@rightbluelabs.com>
2020-01-25 19:01:34 -0700

    - Change a few things

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -14,6 +14,8 @@ class UserDashboard < Administrate::BaseDashboard
     reset_password_token: Field::String,
     reset_password_sent_at: Field::DateTime,
     remember_created_at: Field::DateTime,
+    first_name: Field::String,
+    last_name: Field::String,
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
     admin: Field::Boolean,
@@ -26,6 +28,8 @@ class UserDashboard < Administrate::BaseDashboard
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
   id
+  first_name
+  last_name
   email
   reset_password_token
   admin
@@ -35,6 +39,8 @@ class UserDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
   id
+  first_name
+  last_name
   email
   encrypted_password
   reset_password_token
@@ -49,6 +55,8 @@ class UserDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+  first_name
+  last_name
   email
   encrypted_password
   reset_password_token
